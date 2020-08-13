4de626fad33a96d129106b3f0865e6a088d9d6ce
Denzel <denzel@pop-os.localdomain>
2020-08-12 20:35:44 -0400

    Add and Delete from UI

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/appNavbar'; 
 import './App.css';
-
+import ShoppingList from './components/shoppingList'; 
 function App() {
   return (
     <div className="App">
      <AppNavbar/>
      <h1>Hello</h1>
+     <ShoppingList/>
     </div>
   );
 }
