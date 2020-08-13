53ff7e4627daf614903486d44f78820a6e51b8fa
Denzel <denzel@pop-os.localdomain>
2020-08-13 00:28:26 -0400

    Redux but With Errors

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AppNavbar from './components/appNavbar'; 
 import './App.css';
 import ShoppingList from './components/shoppingList'; 
+import {Provider} from 'react-redux'; 
+import store from './store'; 
+
 function App() {
   return (
-    <div className="App">
-     <AppNavbar/>
-     <h1>Hello</h1>
-     <ShoppingList/>
+    <Provider store={store}>
+      <div className="App">
+      <AppNavbar/>
+      <h1>Hello</h1>
+      <ShoppingList/>
     </div>
+    </Provider>
   );
 }
 
